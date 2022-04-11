6b5cf77c16f921f6d0c23344bc7ca018df4e5547
Axel Rueda <axelcuer@gmail.com>
2022-04-11 18:51:53 -0300

    documentando

--- divide_xy.m
+++ divide_xy.m
@@ -1,12 +1,9 @@
+% resive una fila de las posiciones de la particula en un tiempo t
+% retorna un vector con las posiciones de (x) y otro con las posiciones de (y)
+% para facilitar el ploteo de las particulas
 function [x y] = divide_xy(vec)
-  x = [];
-  y = [];
-  for i = 1:length(vec)
-    if (mod(i,2))
-      x = [x vec(i)];
-    else
-      y = [y vec(i)];
-    end
-  end
+
+  x = vec([1:2:length(vec)]);
+  y = vec([2:2:length(vec)]);
 
 end
